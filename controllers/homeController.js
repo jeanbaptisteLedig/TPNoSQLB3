7557b69e8a0f7fb8f51e51e3369d861dea85dd01
sylvainBrunet <sylvain69740@gmail.com>
2017-02-07 10:18:40 +0100

    hello from the other skys

---
+++ controllers/homeController.js
@@ -0,0 +1,43 @@
+var router = require('express').Router();
+var Person = require('../models/Person');
+
+// Permet de créer une route qui map l'url "/" en GET
+router.get('/', function(req, res) {
+    // Permet de retrouver des résultats sur un modèle
+    Person.find({}).then(function(persons) {
+        // Permet d'afficher une vue et de lui passer des paramètres
+        res.render('home.ejs', { persons: persons});
+    });
+
+});
+
+// Permet de créer une route qui map l'url "/hello" en GET
+router.get('/hello', function(req, res) {
+    var p = new Person({
+        firstname: 'Ted',
+        lastname: 'Mosby',
+        age: 30
+    });
+
+    // Permet d'insérer une nouvelle donnée
+    p.save().then(function(personSaved){
+        res.render('hello.ejs', personSaved);
+    });
+});
+
+router.get('/add', function(req, res) {
+    res.render('add.ejs');
+});
+
+router.post('/add', function(req, res) {
+    res.redirect('/add');
+});
+
+router.get('/list', function(req, res) {
+    res.render('list.ejs');
+});
+
+router.post('/list', function(req, res) {
+    res.redirect('/list');
+});
+module.exports = router;
